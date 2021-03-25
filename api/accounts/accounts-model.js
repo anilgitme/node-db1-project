fe342f2310211dbe1f9f2a47f9ffe87cc7773a66
Anil_Ramcharran <anil.gitme@gmail.com>
2021-03-24 23:10:33 -0400

    working on accounts model

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,27 +1,35 @@
+const db = require('../../data/db-config');
+
 const getAll = () => {
-  // DO YOUR MAGIC
+    // DO YOUR MAGIC
+    return db('accounts');
 }
 
 const getById = id => {
-  // DO YOUR MAGIC
+    // DO YOUR MAGIC
+    const getAccount = db('accounts').where('id', id)
+    return getAccount
 }
 
 const create = async account => {
-  // DO YOUR MAGIC
+    // DO YOUR MAGIC
+    const [id] = await db('accounts').insert(account);
+    const [addId] = await getId(id)
+    return addId
 }
 
-const updateById = async (id, account) => {
-  // DO YOUR MAGIC
+const updateById = async(id, account) => {
+    // DO YOUR MAGIC
 }
 
 const deleteById = async id => {
-  // DO YOUR MAGIC
+    // DO YOUR MAGIC
 }
 
 module.exports = {
-  getAll,
-  getById,
-  create,
-  updateById,
-  deleteById,
+    getAll,
+    getById,
+    create,
+    updateById,
+    deleteById,
 }
