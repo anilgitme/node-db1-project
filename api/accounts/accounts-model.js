f9ec1a9b3f1c2763bc9ebc8df99334d6e935599b
Anil_Ramcharran <anil.gitme@gmail.com>
2021-03-24 23:57:16 -0400

    finished some middleware code still working on it

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -14,16 +14,21 @@ const getById = id => {
 const create = async account => {
     // DO YOUR MAGIC
     const [id] = await db('accounts').insert(account);
-    const [addId] = await getId(id)
+    const [addId] = await getById(id)
     return addId
 }
 
 const updateById = async(id, account) => {
     // DO YOUR MAGIC
+    const updateAcc = await db('accounts').where('id', id).update(account)
+    const [update] = await getById(id)
+    return update;
 }
 
 const deleteById = async id => {
     // DO YOUR MAGIC
+    const deleteAcc = await db('accounts').where('id', id).del();
+    return deleteAcc;
 }
 
 module.exports = {
